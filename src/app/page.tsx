38369e279539c331b2c88ac3437a4d77d0794745
Obed47 <obedwirngo47@gmail.com>
2025-06-01 01:50:18 +0100

    main frontend code here initialised and first push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -12,6 +12,8 @@ export default function Home() {
           height={38}
           priority
         />
+        <Link href="/pages">        <button>go to new page</button>
+ </Link>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
